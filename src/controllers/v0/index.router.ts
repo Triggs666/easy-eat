78c626ce728f2c872fe57559e45421ea83d74180
Triggs666 <oscar.triguero@gmail.com>
2021-07-19 20:44:22 +0200

    19/07-20:44

--- src/controllers/v0/index.router.ts
+++ src/controllers/v0/index.router.ts
@@ -1,9 +1,11 @@
 import {Router, Request, Response} from 'express';
+import { DishRouter } from './test/dish.router';
 import {RestaurantRouter} from './test/rest.router';
 
 const router: Router = Router();
 
 router.use('/rest', RestaurantRouter);
+router.use('/rest/:idRest/dish', DishRouter);
 
 router.get('/', async (req: Request, res: Response) => {
   res.send(`V0`);
