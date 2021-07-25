cb947ab556cdadae2da9cbc504b0ec297b5e9749
Triggs666 <oscar.triguero@gmail.com>
2021-07-25 18:15:46 +0200

    first upload to development branch

--- set_env_capstone.sh
+++
@@ -1,6 +0,0 @@
-export RESTAURANT_TABLE=ot-aws-dyndb-restaurantTable
-export DISH_TABLE=ot-aws-dyndb-dishTable
-export CART_TABLE=ot-aws-dyndb-cartTable
-export AWS_REGION=eu-west-1
-export AWS_PROFILE=default
-
